9b3654b4544c6c82b567bdaa0675b95962ef9384
lunaticbonnie <lunaticbonnie@gmail.com>
2026-05-26 00:37:10 +0200

    c

--- overrides/src/main/java/patrolin/stablefps/mixin/1.15-FPSLimiterMixin.java
+++ overrides/src/main/java/patrolin/stablefps/mixin/1.15-FPSLimiterMixin.java
@@ -22,17 +22,17 @@ public class FPSLimiterMixin {
 		double dt = (1.0 / target_fps) + 1e-9;
 		double nextDrawTime = lastDrawTime + dt;
 		double now_s = System.nanoTime() / 1e9;
-		double remaining_timeout = nextDrawTime - now_s;
-		if (remaining_timeout < -dt) {
+		double remaining_s = nextDrawTime - now_s;
+		if (remaining_s < -dt) {
 			/* NOTE: we fell too far behind, restart */
 			nextDrawTime = now_s;
 		}
 		// wait until the correct time
 		do {
 			// NOTE: wait for OS input events with a positive timeout, or zero timeout if we're behind
-			GLFW.glfwWaitEventsTimeout(Math.max(remaining_timeout, 0));
-			remaining_timeout = nextDrawTime - (System.nanoTime() / 1e9);
-		} while (remaining_timeout > 0);
+			GLFW.glfwWaitEventsTimeout(Math.max(remaining_s, 0));
+			remaining_s = nextDrawTime - (System.nanoTime() / 1e9);
+		} while (remaining_s > 0);
 		lastDrawTime = nextDrawTime;
 	}
 }
